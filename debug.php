6f62ae2ec1bcd152e1ca95dcd8b4e4df80a73a8f
Nullivex <contact@nullivex.com>
2013-02-19 17:42:46 -0800

    fixes #74 and parts of #72 also some of #73

---
+++ debug.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * LSS Core
+ * OpenLSS - Light, sturdy, stupid simple
+ * 2010 Nullivex LLC, All Rights Reserved.
+ * Bryan Tong <contact@nullivex.com>
+ *
+ *   OpenLSS is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   OpenLSS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with OpenLSS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//prints web readable debug output when not in CLI mode
+//	exact same prototype as PHPs var_dump
+function debug_dump(){
+	if(php_sapi_name() == 'cli'){
+		echo '<pre>'; call_user_func_array('var_dump',func_get_args()); echo '</pre>';
+	} else {
+		call_user_func_array('var_dump',func_get_args());
+	}
+}
+
+
