bd9631074774b4d18d829141fc508cb5754c81e9
Nullivex <contact@nullivex.com>
2013-03-13 13:29:40 -0700

    added tests for all the functions

---
+++ test/debugTest.php
@@ -0,0 +1,15 @@
+<?php
+require_once(dirname(__DIR__).'/test_common.php');
+
+class FuncDebugTest extends PHPUNIT_Framework_TestCase {
+
+	public function testDebugDump(){
+		ob_end_flush();
+		ob_start();
+		debug_dump(false,true,3,5,'this is a string');
+		$out = trim(ob_get_contents());
+		ob_clean();
+		$this->assertEquals('5447a8b60d5bc9667e6be2040be3f5b786fcc99b',sha1($out));
+	}
+
+}
